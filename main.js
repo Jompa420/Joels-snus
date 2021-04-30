a00e4bd7399351c1a05fc648b19562336f9e961e
Jompa420 <joel.tengman@elev.ga.ntig.se>
2021-04-30 10:40:37 +0200

    first comitt

---
+++ main.js
@@ -0,0 +1,10 @@
+function showGranit (snus) {
+document.querySelector(".Snus").classList.toggle("hidden");
+document.querySelector("#granit").classList.toggle("hidden");
+}
+
+function goBackGranit (menu) {
+    document.querySelector("#granit").classList.toggle("hidden");
+    document.querySelector(".Snus").classList.toggle("hidden");
+}
+
